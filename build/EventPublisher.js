b59aca136fc56a54cfee3fbedca6287f9cad1826
garrett <dhtmlkitchen@gmail.com>
2009-03-18 23:14:13 -0800

    fix build so copy depends="js.clean". :

--- build/EventPublisher.js
+++ build/EventPublisher.js
@@ -1,21 +1,21 @@
-/**
- * @fileoverview
+/** 
+ * @fileoverview 
  * EventPublisher
  *
  * Released under Academic Free Licence 3.0.
  * @author Garrett Smith
- * @class
+ * @class 
  * <code>APE.EventPublisher</code> can be used for native browser events or custom events.
  *
  * <p> For native browser events, use <code>APE.EventPublisher</code>
- * steals the event handler off native elements and creates a callStack.
+ * steals the event handler off native elements and creates a callStack. 
  * that fires in its place.
  * </p>
  * <p>
  * There are two ways to create custom events.
  * </p>
  * <ol>
- * <li>Create a function on the object that fires the "event", then call that function
+ * <li>Create a function on the object that fires the "event", then call that function 
  * when the event fires (this happens automatically with native events).
  * </li>
  * <li>
@@ -28,11 +28,11 @@
  * This makes it possible to add AOP before advice to the callStack.
  * </p><p>
  * adding before-before advice is possible, but will impair performance.
- * Instead, add multiple beforeAdvice with:
+ * Instead, add multiple beforeAdvice with: 
  * <code>publisher.addBefore(fp, thisArg).add(fp2, thisArg);</code>
  * </p><p>
- * There are no <code>beforeEach</code> and <code>afterEach</code> methods; to create advice
- * for each callback would require modification
+ * There are no <code>beforeEach</code> and <code>afterEach</code> methods; to create advice 
+ * for each callback would require modification 
  * to the registry (see comments below). I have not yet found a real need for this.
  * </p>
  */
@@ -43,14 +43,14 @@
  */
 APE.EventPublisher = function(src, type) {
     this.src = src;
-    // Really could use a List of bound methods here.
+    // Really could use a List of bound methods here. 
     this._callStack = [];
     this.type = type;
 };
 
 APE.EventPublisher.prototype = {
 
-/**
+/**  
  *  @param {Function} fp the callback function that gets called when src[sEvent] is called.
  *  @param {Object} thisArg the context that the function executes in.
  *  @return {EventPublisher} this;
@@ -59,27 +59,27 @@ APE.EventPublisher.prototype = {
         this._callStack.push([fp, thisArg||this.src]);
         return this;
     },
-/**  Adds beforeAdvice to the callStack. This fires before the callstack.
+/**  Adds beforeAdvice to the callStack. This fires before the callstack. 
  *  @param {Function:boolean} fp the callback function that gets called when src[sEvent] is called.
  *  function's returnValue proceed false stops the callstack and returns false to the original call.
  *  @param {Object} thisArg the context that the function executes in.
  *  @return {EventPublisher} this;
  */
     addBefore : function(f, thisArg) {
-        return APE.EventPublisher.add(this, "beforeFire", f, thisArg);
+        return APE.EventPublisher.add(this, "beforeFire", f, thisArg); 
     },
-
-/**  Adds afterAdvice to the callStack. This fires after the callstack.
+    
+/**  Adds afterAdvice to the callStack. This fires after the callstack. 
  *  @param {Function:boolean} fp the callback function that gets called when src[sEvent] is called.
  *  function's returnValue of false returns false to the original call.
  *  @param {Object} thisArg the context that the function executes in.
  *  @return {EventPublisher} this;
  */
     addAfter : function(f, thisArg) {
-        return APE.EventPublisher.add(this, "afterFire", f, thisArg);
+        return APE.EventPublisher.add(this, "afterFire", f, thisArg); 
     },
 
-    /**
+    /** 
      * @param {String} "beforeFire", "afterFire" conveneince.
      * @return {EventPublisher} this;
      */
@@ -95,7 +95,7 @@ APE.EventPublisher.prototype = {
     remove : function(fp, thisArg) {
         var cs = this._callStack, i = 0, len, call;
         if(!thisArg) thisArg = this.src;
-        for( ; i < cs.length; i++) {
+        for(len = cs.length; i < len; i++) {
             call = cs[i];
             if(call[0] === fp && call[1] === thisArg) {
                 return cs.splice(i, 1);
@@ -135,7 +135,7 @@ APE.EventPublisher.prototype = {
     }
 };
 
-/**
+/** 
  *  @static
  *  @param {Object} src the object which calls the function
  *  @param {String} sEvent the function that gets called.
@@ -146,7 +146,7 @@ APE.EventPublisher.add = function(src, sEvent, fp, thisArg) {
     return APE.EventPublisher.get(src, sEvent).add(fp, thisArg);
 };
 
-/**
+/** 
  * @static
  * @private
  * @memberOf {APE.EventPublisher}
@@ -155,12 +155,13 @@ APE.EventPublisher.add = function(src, sEvent, fp, thisArg) {
 APE.EventPublisher.fire = function(publisher) {
     // This closure sucks. We should have partial/bind in ES.
     // If we did, this could more reasonably be a prototype method.
-
+    
     // return function w/identifier doesn't work in Safari 2.
-    return fireEvent;
+    return fireEvent; 
     function fireEvent(e) {
         var preventDefault = false,
-            i = 0, cs = publisher._callStack, csi;
+            i = 0, len,
+            cs = publisher._callStack, csi;
 
         // beforeFire can affect return value.
         if(typeof publisher.beforeFire == "function") {
@@ -170,10 +171,8 @@ APE.EventPublisher.fire = function(publisher) {
             } catch(ex){APE.deferError(ex);}
         }
 
-        // Must check length each time, because a callback
-        // may remove itself (or another callback).
-        for( ; i < cs.length; i++) {
-            csi = cs[i];
+        for(len = cs.length; i < len; i++) {
+            csi = cs[i]; 
             // If an error occurs, continue the event fire,
             // but still throw the error.
             try {
@@ -195,7 +194,7 @@ APE.EventPublisher.fire = function(publisher) {
     }
 };
 
-/**
+/** 
  * @static
  * @param {Object} src the object which calls the function
  * @param {String} sEvent the function that gets called.
@@ -205,18 +204,18 @@ APE.EventPublisher.fire = function(publisher) {
  */
 APE.EventPublisher.get = function(src, sEvent) {
 
-    var publisherList = this.Registry.hasOwnProperty(sEvent) && this.Registry[sEvent] ||
+    var publisherList = this.Registry.hasOwnProperty(sEvent) && this.Registry[sEvent] || 
         (this.Registry[sEvent] = []),
         i = 0, len = publisherList.length,
         publisher;
-
+    
     for(; i < len; i++)
         if(publisherList[i].src === src)
             return publisherList[i];
-
+    
     // not found.
     publisher = new APE.EventPublisher(src, sEvent);
-    // Steal.
+    // Steal. 
     if(src[sEvent])
         publisher.add(src[sEvent], src);
     src[sEvent] = this.fire(publisher);
@@ -224,7 +223,7 @@ APE.EventPublisher.get = function(src, sEvent) {
     return publisher;
 };
 
-/**
+/** 
  * Map of [APE.EventPublisher], keyed by type.
  * @private
  * @static
@@ -235,8 +234,8 @@ APE.EventPublisher.Registry = {};
 /**
  * @static
  * @memberOf {APE.EventPublisher}
- * called onunload, automatically onunload.
- * This is only called for if window.CollectGarbage is
+ * called onunload, automatically onunload. 
+ * This is only called for if window.CollectGarbage is 
  * supported. IE has memory leak problems; other browsers have fast forward/back,
  * but that won't work if there's an onunload handler.
  */
--- build/ape-ep-dom.js
+++ build/ape-ep-dom.js
@@ -235,24 +235,24 @@ if(typeof window != "undefined" && hasOwnProperty && !hasOwnProperty.call(window
         return hasOwnProperty.call(this, p);
     };
 }
-})();/**
- * @fileoverview
+})();/** 
+ * @fileoverview 
  * EventPublisher
  *
  * Released under Academic Free Licence 3.0.
  * @author Garrett Smith
- * @class
+ * @class 
  * <code>APE.EventPublisher</code> can be used for native browser events or custom events.
  *
  * <p> For native browser events, use <code>APE.EventPublisher</code>
- * steals the event handler off native elements and creates a callStack.
+ * steals the event handler off native elements and creates a callStack. 
  * that fires in its place.
  * </p>
  * <p>
  * There are two ways to create custom events.
  * </p>
  * <ol>
- * <li>Create a function on the object that fires the "event", then call that function
+ * <li>Create a function on the object that fires the "event", then call that function 
  * when the event fires (this happens automatically with native events).
  * </li>
  * <li>
@@ -265,11 +265,11 @@ if(typeof window != "undefined" && hasOwnProperty && !hasOwnProperty.call(window
  * This makes it possible to add AOP before advice to the callStack.
  * </p><p>
  * adding before-before advice is possible, but will impair performance.
- * Instead, add multiple beforeAdvice with:
+ * Instead, add multiple beforeAdvice with: 
  * <code>publisher.addBefore(fp, thisArg).add(fp2, thisArg);</code>
  * </p><p>
- * There are no <code>beforeEach</code> and <code>afterEach</code> methods; to create advice
- * for each callback would require modification
+ * There are no <code>beforeEach</code> and <code>afterEach</code> methods; to create advice 
+ * for each callback would require modification 
  * to the registry (see comments below). I have not yet found a real need for this.
  * </p>
  */
@@ -280,14 +280,14 @@ if(typeof window != "undefined" && hasOwnProperty && !hasOwnProperty.call(window
  */
 APE.EventPublisher = function(src, type) {
     this.src = src;
-    // Really could use a List of bound methods here.
+    // Really could use a List of bound methods here. 
     this._callStack = [];
     this.type = type;
 };
 
 APE.EventPublisher.prototype = {
 
-/**
+/**  
  *  @param {Function} fp the callback function that gets called when src[sEvent] is called.
  *  @param {Object} thisArg the context that the function executes in.
  *  @return {EventPublisher} this;
@@ -296,27 +296,27 @@ APE.EventPublisher.prototype = {
         this._callStack.push([fp, thisArg||this.src]);
         return this;
     },
-/**  Adds beforeAdvice to the callStack. This fires before the callstack.
+/**  Adds beforeAdvice to the callStack. This fires before the callstack. 
  *  @param {Function:boolean} fp the callback function that gets called when src[sEvent] is called.
  *  function's returnValue proceed false stops the callstack and returns false to the original call.
  *  @param {Object} thisArg the context that the function executes in.
  *  @return {EventPublisher} this;
  */
     addBefore : function(f, thisArg) {
-        return APE.EventPublisher.add(this, "beforeFire", f, thisArg);
+        return APE.EventPublisher.add(this, "beforeFire", f, thisArg); 
     },
-
-/**  Adds afterAdvice to the callStack. This fires after the callstack.
+    
+/**  Adds afterAdvice to the callStack. This fires after the callstack. 
  *  @param {Function:boolean} fp the callback function that gets called when src[sEvent] is called.
  *  function's returnValue of false returns false to the original call.
  *  @param {Object} thisArg the context that the function executes in.
  *  @return {EventPublisher} this;
  */
     addAfter : function(f, thisArg) {
-        return APE.EventPublisher.add(this, "afterFire", f, thisArg);
+        return APE.EventPublisher.add(this, "afterFire", f, thisArg); 
     },
 
-    /**
+    /** 
      * @param {String} "beforeFire", "afterFire" conveneince.
      * @return {EventPublisher} this;
      */
@@ -332,7 +332,7 @@ APE.EventPublisher.prototype = {
     remove : function(fp, thisArg) {
         var cs = this._callStack, i = 0, len, call;
         if(!thisArg) thisArg = this.src;
-        for( ; i < cs.length; i++) {
+        for(len = cs.length; i < len; i++) {
             call = cs[i];
             if(call[0] === fp && call[1] === thisArg) {
                 return cs.splice(i, 1);
@@ -372,7 +372,7 @@ APE.EventPublisher.prototype = {
     }
 };
 
-/**
+/** 
  *  @static
  *  @param {Object} src the object which calls the function
  *  @param {String} sEvent the function that gets called.
@@ -383,7 +383,7 @@ APE.EventPublisher.add = function(src, sEvent, fp, thisArg) {
     return APE.EventPublisher.get(src, sEvent).add(fp, thisArg);
 };
 
-/**
+/** 
  * @static
  * @private
  * @memberOf {APE.EventPublisher}
@@ -392,12 +392,13 @@ APE.EventPublisher.add = function(src, sEvent, fp, thisArg) {
 APE.EventPublisher.fire = function(publisher) {
     // This closure sucks. We should have partial/bind in ES.
     // If we did, this could more reasonably be a prototype method.
-
+    
     // return function w/identifier doesn't work in Safari 2.
-    return fireEvent;
+    return fireEvent; 
     function fireEvent(e) {
         var preventDefault = false,
-            i = 0, cs = publisher._callStack, csi;
+            i = 0, len,
+            cs = publisher._callStack, csi;
 
         // beforeFire can affect return value.
         if(typeof publisher.beforeFire == "function") {
@@ -407,10 +408,8 @@ APE.EventPublisher.fire = function(publisher) {
             } catch(ex){APE.deferError(ex);}
         }
 
-        // Must check length each time, because a callback
-        // may remove itself (or another callback).
-        for( ; i < cs.length; i++) {
-            csi = cs[i];
+        for(len = cs.length; i < len; i++) {
+            csi = cs[i]; 
             // If an error occurs, continue the event fire,
             // but still throw the error.
             try {
@@ -432,7 +431,7 @@ APE.EventPublisher.fire = function(publisher) {
     }
 };
 
-/**
+/** 
  * @static
  * @param {Object} src the object which calls the function
  * @param {String} sEvent the function that gets called.
@@ -442,18 +441,18 @@ APE.EventPublisher.fire = function(publisher) {
  */
 APE.EventPublisher.get = function(src, sEvent) {
 
-    var publisherList = this.Registry.hasOwnProperty(sEvent) && this.Registry[sEvent] ||
+    var publisherList = this.Registry.hasOwnProperty(sEvent) && this.Registry[sEvent] || 
         (this.Registry[sEvent] = []),
         i = 0, len = publisherList.length,
         publisher;
-
+    
     for(; i < len; i++)
         if(publisherList[i].src === src)
             return publisherList[i];
-
+    
     // not found.
     publisher = new APE.EventPublisher(src, sEvent);
-    // Steal.
+    // Steal. 
     if(src[sEvent])
         publisher.add(src[sEvent], src);
     src[sEvent] = this.fire(publisher);
@@ -461,7 +460,7 @@ APE.EventPublisher.get = function(src, sEvent) {
     return publisher;
 };
 
-/**
+/** 
  * Map of [APE.EventPublisher], keyed by type.
  * @private
  * @static
@@ -472,8 +471,8 @@ APE.EventPublisher.Registry = {};
 /**
  * @static
  * @memberOf {APE.EventPublisher}
- * called onunload, automatically onunload.
- * This is only called for if window.CollectGarbage is
+ * called onunload, automatically onunload. 
+ * This is only called for if window.CollectGarbage is 
  * supported. IE has memory leak problems; other browsers have fast forward/back,
  * but that won't work if there's an onunload handler.
  */
