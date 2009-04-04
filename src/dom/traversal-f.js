c315833556ccd3389ff9bc043b5ee8757ce57cbd
garrett <dhtmlkitchen@gmail.com>
2009-04-04 13:37:22 -0800

    Filter out comment nodes (for IE) plus small optimizations.

--- src/dom/traversal-f.js
+++ src/dom/traversal-f.js
@@ -1,8 +1,11 @@
 APE.namespace("APE.dom");
 (function(){
 
-    var docEl = document.documentElement;
-
+    var docEl = document.documentElement,
+        tagName = "tagName",
+        caseTransform = /^H/.test(docEl[tagName]) ? 'toUpperCase' : 'toLowerCase',
+        tagExp = /^[A-Z]/;
+        
     APE.mixin(
         APE.dom, {
         contains : getContains(),
@@ -20,21 +23,25 @@ APE.namespace("APE.dom");
      * code body.contains(body); 
      * Whereas APE.dom.contains will return false.
      */
+
     function getContains(){
         if('compareDocumentPosition'in docEl)
             return function(el, b) {
                 return (el.compareDocumentPosition(b) & 16) !== 0;
         };
-        else if(docEl.contains)
+        else if('contains'in docEl)
             return function(el, b) {
                 return el !== b && el.contains(b);
         };
         return function(el, b) {
             if(el === b) return false;
-            while(el != b && (b = b.parentNode) != null);
+            while(el != b && (b = b.parentNode) !== null);
             return el === b;
         };
     }
+
+    //docEl = null;
+
     /** 
      * @memberOf APE.dom
      * @param {HTMLElement} el the element to start from.
@@ -57,33 +64,34 @@ APE.namespace("APE.dom");
         return null;
     }
 
-    var isUpperCase = /^H/.test(docEl.tagName);
-    function findAncestorWithTagName(el, tagName) {
-        tagName = (isUpperCase ? tagName.toUpperCase() : tagName.toLowerCase());
-        for(var parent = el.parentNode;parent != null; ){
-            if( parent.tagName === tagName )
+    function findAncestorWithTagName(el, tag) {
+        tag = tag[caseTransform]();
+        for(var parent = el.parentNode;parent !== null; ){
+            if( parent[tagName] === tag )
                 return parent;
             parent = parent.parentNode;
         }
         return null;
     }
 
+    /** Filter out text nodes and, in IE, comment nodes. */
     function findNextSiblingElement(el) {
-        for(var ns = el.nextSibling; ns != null; ns = ns.nextSibling)
-            if(ns.tagName) 
+        for(var ns = el.nextSibling; ns !== null; ns = ns.nextSibling)
+            if(tagName in ns && tagExp.test(ns[tagName])) 
                 return ns;
-            return null;
+        return null;
     }
 
     function findPreviousSiblingElement(el) {
-        for(var ps = el.previousSibling; ps != null; ps = ps.previousSibling)
-            if(ps.tagName) 
+        for(var ps = el.previousSibling; ps !== null; ps = ps.previousSibling) {
+            if(tagName in ps && tagExp.test(ps[tagName])) 
                 return ps;
-            return null;
+        }
+        return null;
     }
 
     function getChildElements(el) {
-        var i = 0, ret = [], len, tagName,
+        var i = 0, ret = [], len, tag,
             cn = el.children || el.childNodes, c;
         
         // IE throws error when calling 
@@ -91,8 +99,8 @@ APE.namespace("APE.dom");
         // IE also includes comment nodes.
         for(len = cn.length; i < len; i++) {
             c = cn[i];
-            tagName = c.tagName;
-            if(typeof tagName !== "string" || tagName === "!") continue;
+            tag = c[tagName];
+            if(typeof tag !== "string" || tag === "!") continue;
             ret[ret.length] = c;
         }
         return ret;
