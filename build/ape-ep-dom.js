3b522d5d191205cf91f7b5d9b83d03b3abf07404
GarrettS <dhtmlkitchen@gmail.com>
2009-08-29 14:11:06 -0700

    Refactor EventPublisher to Closure.

--- build/ape-ep-dom.js
+++ build/ape-ep-dom.js
@@ -269,13 +269,8 @@
  * <code>fire</code>, et c.
  */
 (function(){
-/** 
- * Map of [APE.EventPublisher], keyed by type.
- * @private
- * @static
- * @memberOf {APE.EventPublisher}
- */
 var APE = self.APE,
+   /** Map of [APE.EventPublisher], keyed by type. */
     Registry = {};
 
 APE.EventPublisher = EventPublisher;
