ef4d1af56ec5c96d4cb2438a71ddf14c65fe8d1d
GarrettS <dhtmlkitchen@gmail.com>
2009-09-09 01:34:55 -0700

    remove to captilize directory names: "Calendar", "Scroller"

--- build/widget/calendar/Calendar_en-min.js
+++
@@ -1,1 +0,0 @@
-APE.namespace("APE.widget");APE.widget.CalendarLocale={days:{abbr:["Su","M","Tu","W","Th","F","Sa"]},months:{full:["January","February","March","April","May","June","July","August","September","October","November","December"]},nextMonth:"next month",previousMonth:"previous month",nextYear:"next year",previousYear:"previous year"};
