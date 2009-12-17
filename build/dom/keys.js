a4a5a861029eb7fb4d42da02faf527f6d3f45cde
GarrettS <dhtmlkitchen@gmail.com>
2009-12-17 09:09:39 -0800

    keys.js - Missed a few files in "/build" in last checkin.

---
+++ build/dom/keys.js
@@ -0,0 +1,12 @@
+APE.namespace("APE.dom");
+APE.dom.key = {
+    LEFT : 37,
+    UP : 38,
+    RIGHT : 39,
+    DOWN : 40,
+    
+    ARROW_KEY_EXP : /^(?:37|38|39|40)$/,
+    ENTER : 13,
+    TAB : 9,
+    ESC : 27
+};
