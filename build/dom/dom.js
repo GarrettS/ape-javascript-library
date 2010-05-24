7604d54c38e8b7214a47ccc81a608e80d811dbe2
Garrett Smith <dhtmlkitchen@gmail.com>
2010-05-24 15:32:27 -0700

    Moved function to dom.js. Shortened scope chain (simplifies & reduces size) -- alias parseInt. Changed `this` to `dom` to allow unqualified reference (also helps reduce size).
    Moved test to separate test file.
    
    Tested IE 5.5-9. Safari 4, Opera 10.5. Firefox 2, 3.6, Chrome 4.

--- build/dom/dom.js
+++ build/dom/dom.js
@@ -1396,4 +1396,29 @@ APE.namespace("APE.dom");
             return val;
         }
     }
-})();
+})();/**
+ * @requires APE.dom.style-f.js
+ */
+APE.dom.getPixelCoords = function (el){
+    var ret, dom = APE.dom,
+        parseInt = self.parseInt,
+        f = (dom.IS_COMPUTED_STYLE_SUPPORTED ? function(el) {
+        var cs = el[dom.OWNER_DOCUMENT].defaultView.getComputedStyle(el, "");
+        return{
+            x : parseInt(cs.left, 10)||0,
+            y : parseInt(cs.top, 10)||0
+        };
+    } : function(el){
+        var style = el.style;
+        return{
+            // pixelLeft, in IE returns 0 when the element does not have 
+            // left: in the style attribute, so if that fails, try to read 
+            // the style using dom.getStyle.
+            x : style.pixelLeft || parseInt(dom.getStyle(el,"left"), 10)||0,
+            y : style.pixelTop || parseInt(dom.getStyle(el,"top"), 10)||0
+        };
+    });
+    ret = (dom.getPixelCoords = f)(el);
+    el = null;
+    return ret;
+};
