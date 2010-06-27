455febebd95a371eab0f253c91d5e6f4d6b753f8
Garrett Smith <dhtmlkitchen@gmail.com>
2010-06-27 02:10:02 -0700

    constants for modes which APE has not been tested under (not officially supported): IS_XML_DOM and IS_QUIRKS_MODE. Cross-frame checks using APE.dom.getConstants(document);

--- build/dom/dom.js
+++ build/dom/dom.js
@@ -1,24 +1,37 @@
-APE.namespace("APE.dom").mixin(function(){
-	var od = "ownerDocument",
-	    doc = document,
-	    docEl = doc.documentElement,
-    	view = doc.defaultView;
+// Cross-frame feature test function that returns an object, 
+// constants for currently executing document added to APE.dom.
+APE.namespace("APE.dom").getConstants = function(doc) {
+    doc = doc || document;
+    var od = "ownerDocument",
+        docEl = doc.documentElement,
+        UNDEFINED = "undefined",
+        view = doc.defaultView,
+        compatMode = doc.compatMode,
+        testStyle,
+        IS_STANDARDS_MODE = compatMode ? compatMode != "BackCompat" :
+            ((testStyle = doc.createElement("p").style).width = "1", !testStyle.width);
+        
     return{
         TEXT_CONTENT : typeof docEl.textContent === "string" ? "textContent" : "innerText",
         IS_BODY_ACTING_ROOT : docEl.clientWidth === 0,
         // typeof, not in for BlackBerry9000.
-    	OWNER_DOCUMENT : docEl && typeof docEl[od] !== "undefined" ? od : "document",
-        IS_CURRENT_STYLE_SUPPORTED : typeof docEl.currentStyle != "undefined",
+        OWNER_DOCUMENT : docEl && typeof docEl[od] !== UNDEFINED ? od : "document",
+        IS_CURRENT_STYLE_SUPPORTED : typeof docEl.currentStyle != UNDEFINED,
         
-        IS_COMPUTED_STYLE_SUPPORTED : view && view.getComputedStyle != "undefined",
+        IS_COMPUTED_STYLE_SUPPORTED : !!view && view.getComputedStyle != UNDEFINED,
 
         // TODO: remove deprecated.
-        IS_COMPUTED_STYLE : view && view.getComputedStyle != "undefined",
+        IS_COMPUTED_STYLE : view && view.getComputedStyle != UNDEFINED,
 
         // IE, Safari, and Opera support clientTop. FF 2 doesn't
-        IS_CLIENT_TOP_SUPPORTED : typeof docEl.clientTop != "undefined"
+        IS_CLIENT_TOP_SUPPORTED : typeof docEl.clientTop != UNDEFINED,
+        
+        // Not supported.
+        IS_XML_DOM : docEl.tagName === "html",
+        IS_QUIRKS_MODE : !IS_STANDARDS_MODE
     };
-}());APE.namespace("APE.dom").key = {
+};
+APE.dom.mixin(APE.dom.getConstants(document));APE.namespace("APE.dom").key = {
     LEFT : 37,
     UP : 38,
     RIGHT : 39,
