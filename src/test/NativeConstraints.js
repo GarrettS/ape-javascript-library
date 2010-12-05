9c57356e49c036231a781227876d8b1a8855ab9e
GarrettS <dhtmlkitchen@gmail.com>
2010-12-04 23:40:17 -0800

    Added isFalse, added optional message parameter to all NativeConstraints.

--- src/test/NativeConstraints.js
+++ src/test/NativeConstraints.js
@@ -1,18 +1,27 @@
 APE.test.NativeConstraints = {
-    isTrue : function(value) {
+    isTrue : function(value, message) {
         return function() {
         	return value === true ? "" : 
 	            "found " + value + " (" + (typeof value)  
-	                + "), expected true";
+	                + "), expected true. " + (message || "");
         };
     },
     
-    areSame : function(a, b) {
+    isFalse : function(value, message) {
+        return function() {
+            return value === false ? "" : 
+                "found " + value + " (" + (typeof value)  
+                    + "), expected false. " + (message || "");
+        };        
+    },
+    
+    areSame : function(a, b, message) {
     	return function() {
     		var ret = "";
 	    	if(a !== b) {
 	    		ret = "Actual " + a + " (" + typeof a + ")"
-	    			+ " was not " + b + " (" + typeof b + ")";
+	    			+ " was not " + b + " (" + typeof b + "). " 
+	    			+ (message || "");
 	    		a = b = null;
 	    	}
 	    	return ret;
