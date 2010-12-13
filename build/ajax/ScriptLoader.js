e64280d04295c5dbe9e327e7e4aab9b4ed219555
GarrettS <dhtmlkitchen@gmail.com>
2010-12-12 21:46:09 -0800

    Added permissions to domunit dir for Apache drop-in.

--- build/ajax/ScriptLoader.js
+++ build/ajax/ScriptLoader.js
@@ -1,10 +1,4 @@
-APE.namespace("APE.ajax").mixin({
-    appendToURI : function(baseUri, queryParams) {
-        var ch = baseUri ? baseUri.indexOf("?") !== -1 ? "&" : "?" : "";
-        return (baseUri || "") + ch + queryParams;
-    },
-    jsonp : function(data) { return data; }
-});APE.namespace("APE.ajax").defineFactory(
+APE.namespace("APE.ajax").defineFactory(
     "ScriptLoader", function(ScriptLoader) {
         
         var noop = Function.prototype,
